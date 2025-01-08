ccbc0720b860625e40f7ed45b737dc45b27bdd95
Feelx234 <felix.stamm@rwth-aachen.de>
2025-01-08 14:07:39 +0100

    cleaned config files, pre-commit-hooks

--- tests/test_multivariate_poly.py
+++ tests/test_multivariate_poly.py
@@ -3,13 +3,14 @@ import unittest
 import numpy as np
 from numpy.testing import assert_array_equal
 
-from microagg1d.multivariate_poly import set_both_seeds, forest2
-
+from microagg1d.multivariate_poly import forest2, set_both_seeds
 
 
 class TestMultivariatePoly(unittest.TestCase):
     def get_example_1(self):
-        return np.array([[1,2], [2,0], [1,0], [4,5],[5,3],[3,4]], dtype=np.int64)
+        return np.array(
+            [[1, 2], [2, 0], [1, 0], [4, 5], [5, 3], [3, 4]], dtype=np.int64
+        )
 
     def test_forest2_simple1(self):
         set_both_seeds(2)
@@ -19,11 +20,7 @@ class TestMultivariatePoly(unittest.TestCase):
         assert_array_equal(partitions[0], [1, 0, 2])
         assert_array_equal(partitions[1], [3, 5, 4])
 
-        assert_array_equal(edges, [[0, 1],
-                                    [5, 3],
-                                    [2, 1],
-                                    [4, 5]])
-
+        assert_array_equal(edges, [[0, 1], [5, 3], [2, 1], [4, 5]])
 
     def test_forest2_simple2(self):
         set_both_seeds(3)
@@ -33,10 +30,7 @@ class TestMultivariatePoly(unittest.TestCase):
         assert_array_equal(partitions[0], [1, 2, 0])
         assert_array_equal(partitions[1], [4, 3, 5])
 
-        assert_array_equal(edges, [[2, 1],
-                                    [0, 1],
-                                    [3, 4],
-                                    [5, 3]])
+        assert_array_equal(edges, [[2, 1], [0, 1], [3, 4], [5, 3]])
 
 
 if __name__ == "__main__":
